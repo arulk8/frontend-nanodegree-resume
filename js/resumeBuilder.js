b7d526d2ae81c2b02c9a437628e57b0da5f31b5b
arulk8 <arulmurugan194@gmail.com>
2016-11-15 18:50:52 +0530

    changed a lot

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -23,6 +23,92 @@ var skill =["HTML","Javascripts","C++","C"];
  	"skills" :skill,
  	"welcome_message":"Enthustiatic programmer,who seeks new things",
  }
+
+ var education= {
+    "schools": [
+        {
+       "Name"     : "Madras Institute of technology",
+       "Degree"   : "Bachelore of Engineering",
+       "location" : "Chennai,India",
+       "major"    :"Electronics and Communication"
+     },
+     {
+        "Name"    : "SRV Boys Higher Secondary school",
+       "Degree"   : "12TH grade",
+       "location" : "Salem,India",
+       "major"    : "Higher Secondary"
+     },
+     {
+       "Name"     : "Sri Vidya Mandir Higher Secondary School Rasipuram",
+       "Degree"   : "10TH grade",
+       "location" : "Salem,India",
+       "major"    : "Matriculation Board"
+     }
+  ]
+  };
+
+  var work={
+    "jobs": [
+      {
+       "employer":"Infosys",
+       "title": "system Analyst",
+       "dates": "2016-Present",
+       "location":"Banglore,India",
+       "description":"Not working presently in training"
+      }
+  ]
+ };
+
+ var project={
+      "projects": [
+
+          {
+            "title": " Designing a Responsive Portfolio Site ",
+            "dates": "2016",
+            "Description": "To Design a portfolio site using responsive design"
+
+          },
+
+          {
+            "title": "Ultra Low Power SRAM Design Using 9nm TFET",
+            "dates": "2016",
+            "Description": "To show TFET as superior alternative to MOSFET"
+
+          },
+
+          {
+            "title": "Traffic Congestion Detection in Arterial Road Using RF Technology",
+            "dates": "2015",
+            "Description": "To reduce the traffic in Arterial Road using RF id and adaptive Algorithm"
+          }
+      ]
+
+   };
+
+var onlineclass={
+          "classes": [
+          {
+            "Title" : "Introduction to HTML ans CSS",
+            "school": "udacity",
+            "URL"   : "https://classroom.udacity.com/courses/ud304",
+            "date"  : "2016"
+          },
+          {
+            "Title" : "Basics of Javascripts",
+            "school": "udacity",
+            "URL"   : "https://classroom.udacity.com/courses/ud804",
+            "date"  : "2016"
+          },
+          {
+            "Title" : "Introduction to ComputerScience",
+            "school": "udacity",
+            "URL"   : "https://classroom.udacity.com/courses/cs101",
+            "date"  : "2016"
+          }
+
+          ]
+ };
+
  bio.display=function()
  {
  
@@ -60,48 +146,8 @@ $("#footerContacts").append(formattedmobile,formattedemail,formattedgithub);
 }
 
 
-bio.display();
-  /*var work= {};
-  work.employer = "xyz";
-  work.title = "System Analyst";
-  work.location = "xyz city";
 
-  var formattedworkEmployer=HTMLworkEmployer.replace("%data%",work.employer);
-  var formattedworkTitle=HTMLworkTitle.replace("%data%",work.title);
-  var formattedworkLocation=HTMLworkLocation.replace("%data%",work.location);
-  $("#workExperience").append(HTMLworkStart);
-
-  $("#workExperience").append(formattedworkEmployer);
-  $("#workExperience").append(formattedworkTitle);
-  $("#workExperience").append(formattedworkLocation);*/
-
-  var education= {
-    "schools": [
-        {
-       "Name"     : "Madras Institute of technology",
-       "Degree"   : "Bachelore of Engineering",
-       "location" : "Chennai,India",
-       "major"    :"Electronics and Communication"
-     },
-     {
-        "Name"    : "SRV Boys Higher Secondary school",
-       "Degree"   : "12TH grade",
-       "location" : "Salem,India",
-       "major"    : "Higher Secondary"
-     },
-     {
-       "Name"     : "Sri Vidya Mandir Higher Secondary School Rasipuram",
-       "Degree"   : "10TH grade",
-       "location" : "Salem,India",
-       "major"    : "Matriculation Board"
-     }
-  ]
-  };
   
-  /* school["Name"] = "Madras Institute of technology";
-     school["Degree"] = "Bachelore of Engineering";
-     school["Location"] = "chennai";
-     school["major"]= "Electronics and Communication"; */
 education.display= function()
 {
 
@@ -116,50 +162,40 @@ education.display= function()
   var formattedSchoolandDegree = formattedschoolName + formattedschoolDegree;
  
   //$("#education").append(formattedschoolName);
-  $("#education").append(formattedSchoolandDegree);
-  $("#education").append(formattedschoolLocation);
-  $("#education").append(formattedschoolMajor);
+  $(".education-entry:last").append(formattedSchoolandDegree);
+  $(".education-entry:last").append(formattedschoolLocation);
+  $(".education-entry:last").append(formattedschoolMajor);
  }
  );
 }
 
-education.display();
 
 
-  var work={
-  	"jobs": [
-  		{
-  		 "employer":"Infosys",
-  		 "title": "system Analyst",
-  		 "dates": "2016-Present",
-  		 "location":"Banglore,India",
-  		 "description":"Not working presently in training"
-    	}
- 	]
- };
 
+  
  work.display =function()
  { 
+  $("#workExperience").append(HTMLworkStart);
 
  work.jobs.forEach(function(job)
   {
   	
-  	$("#workExperience").append(HTMLworkStart);
+  	
   	
   	var formattedworkEmployer = HTMLworkEmployer.replace("%data%",job.employer);
   	var formattedworkTitle = HTMLworkTitle.replace("%data%",job.title);
 
     var formattedEmployerTitle=formattedworkEmployer+formattedworkTitle;
-  	$("#workExperience").append(formattedEmployerTitle);
+  	$(".work-entry:last").append(formattedEmployerTitle);
 
     var formattedworkDates = HTMLworkDates.replace("%data%",job.dates);
-    $("#workExperience").append(formattedworkDates);
+    $(".work-entry:last").append(formattedworkDates);
 
   	var formattedworkLocation = HTMLworkLocation.replace("%data%",job.location);
-  	$("#workExperience").append(formattedworkLocation);
+  	$(".work-entry:last").append(formattedworkLocation);
 
   	var formattedworkDescription = HTMLworkDescription.replace("%data%",job.description);
-  	$("#workExperience").append(formattedworkDescription);
+  	$(".work-entry:last").append(formattedworkDescription);
 
   	
   	$(".work-Entry:last").append(formattedEmployerTitle);
@@ -167,111 +203,66 @@ education.display();
   );
 }
 
-work.display();
 
-   var project={
-   	  "projects": [
-          {
-          	"title": "Ultra Low Power SRAM Design Using 9nm TFET",
-          	"dates": "2016",
-          	"Description": "To show TFET as superior alternative to MOSFET"
-
-          },
-          {
-          	"title": "Traffic Congestion Detection in Arterial Road Using RF Technology",
-          	"dates": "2015",
-          	"Description": "To reduce the traffic in Arterial Road using RF id and adaptive Algorithm"
-          }
-   	  ]
-
-   };
-
+   
   project.display = function()
   {
+    $("#projects").append(HTMLprojectStart);
 
    project.projects.forEach( function( i) {
 
-    $("#projects").append(HTMLprojectStart);
+    
   	
   	var formattedprojectTitle = HTMLprojectTitle.replace("%data%",i.title);
-  	$("#projects").append(formattedprojectTitle);
+  	$(".project-entry:last").append(formattedprojectTitle);
 
   	var formattedprojectDates = HTMLprojectDates.replace("%data%",i.dates);
-  	$("#projects").append(formattedprojectDates);
+  	$(".project-entry:last").append(formattedprojectDates);
 
   	var formattedprojectDescription = HTMLprojectDescription.replace("%data%",i.Description);
-  	$("#projects").append(formattedprojectDescription);
+  	$(".project-entry:last").append(formattedprojectDescription);
   	}
 );
 
 }
 
- project.display();
 
- var onlineclass={
-          "classes": [
-          {
-            "Title" : "Introduction to HTML ans CSS",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/ud304",
-            "date"  : "2016"
-          },
-          {
-            "Title" : "Basics of Javascripts",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/ud804",
-            "date"  : "2016"
-          },
-          {
-            "Title" : "Introduction to ComputerScience",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/cs101",
-            "date"  : "2016"
-          }
+ 
 
-          ]
- };
 
-$("#education").append(HTMLonlineClasses);
 
 onlineclass.display =function()
 {
-
+$(".education-entry:last").append(HTMLonlineClasses);
 onlineclass.classes.forEach(function(i)
   {
-
+    
    var formattedonlineTitle = HTMLonlineTitle.replace("%data%",i.Title);
   // $("#education").append(formattedonlineTitle);
 
    var formattedonlineSchool = HTMLonlineSchool.replace("%data%",i.school);
    var TitleandSchool = formattedonlineTitle + formattedonlineSchool;
 
-   $("#education").append(TitleandSchool);
+   $(".education-entry:last").append(TitleandSchool);
 
    var formattedonlineDates = HTMLonlineDates.replace("%data%",i.date);
-   $("#education").append(formattedonlineDates);
+   $(".education-entry:last").append(formattedonlineDates);
 
    var formattedonlineURL = HTMLonlineURL.replace("%data%",i.URL);
-   $("#education").append(formattedonlineURL);
+   $(".education-entry:last").append(formattedonlineURL);
   }
 
 );
 }
+
+
+bio.display();
+education.display();
+work.display();
+project.display();
 onlineclass.display();
 
-
-/* $(document).click(
-function(loc)
-{
-var X= loc.pageX;
-var Y= loc.pageY;
-console.log("X="+ X);
-console.log("Y="+ Y);
-}
-);*/
- $("#main").append(internationalizeButton);
-
-  function inName(name)
+function inName(name)
   {
     var a =[];
      a = name;
@@ -282,4 +273,5 @@ console.log("Y="+ Y);
      return a[0]+" "+a[1];
 
   }
+  $("#main").append(internationalizeButton);
   $("#mapDiv").append(googleMap);
