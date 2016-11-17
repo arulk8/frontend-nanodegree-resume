117823c96005fa3f7ed86a94168f55c067db9173
arulk8 <arulmurugan194@gmail.com>
2016-11-17 19:08:37 +0530

    made changes for resubmission

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,43 +1,134 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-//var awesomeThoughts = " I am arul i am awesome";
-/*var funThoughts = awesomeThoughts.replace("awesome","fun");
-$("#main").append(funThoughts); */
+
 var skill = ["HTML", "Javascripts", "C++", "C"];
 var bio = {
     "name": "Arulmurugan Kanagasabapathi",
     "role": "Web Developer",
     contacts: {
         "email"   : "arulmurugan194@gmail.com",
-        "phone_no": "9003568965",
+        "mobile": "9003568965",
         "github"  : "arulk8",
         "location": "Chennai,India"
     },
 
-    "bioPic": "images/002.jpg",
+    "biopic": "images/002.jpg",
     "skills": skill,
-    "welcome_message": "Enthustiatic programmer,who seeks new things",
+    "welcomeMessage": "Enthustiatic programmer,who seeks new things",
+    "display" : function() {
+
+    var formattedName = HTMLheaderName.replace("%data%", bio.name);
+    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+    $("#header").prepend(formattedRole);
+    $("#header").prepend(formattedName);
+
+    var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+    var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
+    var formattedemail = HTMLemail.replace("%data%", bio.contacts.email);
+    var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
+    var formattedbioPic = HTMLbioPic.replace("%data%", bio.biopic);
+    var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+    $("#topContacts").append(formattedmobile,formattedgithub,formattedemail,formattedlocation);
+    $("#header").append(formattedbioPic);
+    $("#header").append(HTMLskillsStart);
+    for (var i = 0; i < bio.skills.length; i++) {
+        var formattedskills = HTMLskills.replace("%data%", bio.skills[i]);
+        $("#skills").append(formattedskills);
+    }
+    $("#skills").append(formattedMsg);
+
+    $("#footerContacts").append(formattedmobile, formattedemail, formattedgithub);
+}
 }
 
 var education = {
     "schools": [{
-        "Name"    : "Madras Institute of technology",
-        "Degree"  : "Bachelore of Engineering",
+        "name"    : "Madras Institute of technology",
+        "degree"  : "Bachelore of Engineering",
         "location": "Chennai,India",
-        "major"   : "Electronics and Communication"
+        "major"   : "Electronics and Communication",
+        "dates"   : "2012-2016",
+        "url"     :"http://www.mitindia.edu"
     }, {
-        "Name"    : "SRV Boys Higher Secondary school",
-        "Degree"  : "12TH grade",
+        "name"    : "SRV Boys Higher Secondary school",
+        "degree"  : "12TH grade",
         "location": "Salem,India",
-        "major"   : "Higher Secondary"
+        "major"   : "Higher Secondary",
+        "dates"   : "2010-2012",
+        "url"     :"http://www.srvacademy.org/hitechboys"
     }, {
-        "Name"    : "Sri Vidya Mandir Higher Secondary School Rasipuram",
-        "Degree"  : "10TH grade",
+        "name"    : "Sri Vidya Mandir Higher Secondary School Rasipuram",
+        "degree"  : "10TH grade",
         "location": "Salem,India",
-        "major"   : "Matriculation Board"
-    }]
+        "major"   : "Matriculation Board",
+        "dates"   : "2006-2010",
+        "url"     :"http://www.srividyamandirrspm.org/"
+    }
+    
+    ],
+
+    "onlineCourses": [{
+            "title" : "Introduction to HTML ans CSS",
+            "school": "udacity",
+            "url"   : "https://classroom.udacity.com/courses/ud304",
+            "date"  : "2016"
+        }, {
+            "title" : "Basics of Javascripts",
+            "school": "udacity",
+            "url"   : "https://classroom.udacity.com/courses/ud804",
+            "date"  : "2016"
+        }, {
+            "title" : "Introduction to ComputerScience",
+            "school": "udacity",
+            "url"   : "https://classroom.udacity.com/courses/cs101",
+            "date"  : "2016"
+        }
+
+    ],
+
+    "display": function() {
+    $("#education").append(HTMLschoolStart);
+    education.schools.forEach(function(i) {
+
+        var formattedschoolName = HTMLschoolName.replace("%data%", i.name);
+        var formattedschoolDegree = HTMLschoolDegree.replace("%data%", i.degree);
+        var formattedschoolLocation = HTMLschoolLocation.replace("%data%", i.location);
+        var formattedschoolDates = HTMLschoolDates.replace("%data%", i.dates);
+        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", i.major);
+        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", i.major);
+        var formattedschoolURL = HTMLschoolURL.replace("%data%", i.url)
+
+        var formattedSchoolandDegree = formattedschoolName + formattedschoolDegree;
+
+        //$("#education").append(formattedschoolName);
+        $(".education-entry:last").append(formattedSchoolandDegree);
+        $(".education-entry:last").append(formattedschoolLocation);
+        $(".education-entry:last").append(formattedschoolDates);
+        $(".education-entry:last").append(formattedschoolMajor);
+        $(".education-entry:last").append(formattedschoolURL);
+        
+    });
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+    education.onlineCourses.forEach(function(i) {
+
+            var formattedonlineTitle = HTMLonlineTitle.replace("%data%", i.title);
+            // $("#education").append(formattedonlineTitle);
+            var formattedonlineSchool = HTMLonlineSchool.replace("%data%", i.school);
+            var formattedonlineDates = HTMLonlineDates.replace("%data%", i.date);
+            var formattedonlineURL = HTMLonlineURL.replace("%data%", i.url);
+            var TitleandSchool = formattedonlineTitle + formattedonlineSchool;
+
+            $(".education-entry:last").append(TitleandSchool);
+            $(".education-entry:last").append(formattedonlineDates);
+            $(".education-entry:last").append(formattedonlineURL);
+        }
+
+    );
+}
+
 };
+
+  
 
 var work = {
     "jobs": [{
@@ -46,116 +137,10 @@ var work = {
         "dates"   : "2016-Present",
         "location": "Banglore,India",
         "description": "Not working presently in training"
-    }]
-};
 
-var project = {
-    "projects": [
+    }],
 
-        {
-            "title": " Designing a Responsive Portfolio Site ",
-            "dates": "2016",
-            "Description": "To Design a portfolio site using responsive design"
-
-        },
-
-        {
-            "title": "Ultra Low Power SRAM Design Using 9nm TFET",
-            "dates": "2016",
-            "Description": "To show TFET as superior alternative to MOSFET"
-
-        },
-
-        {
-            "title": "Traffic Congestion Detection in Arterial Road Using RF Technology",
-            "dates": "2015",
-            "Description": "To reduce the traffic in Arterial Road using RF id and adaptive Algorithm"
-        }
-    ]
-
-};
-
-var onlineclass = {
-    "classes": [{
-            "Title" : "Introduction to HTML ans CSS",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/ud304",
-            "date"  : "2016"
-        }, {
-            "Title" : "Basics of Javascripts",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/ud804",
-            "date"  : "2016"
-        }, {
-            "Title" : "Introduction to ComputerScience",
-            "school": "udacity",
-            "URL"   : "https://classroom.udacity.com/courses/cs101",
-            "date"  : "2016"
-        }
-
-    ]
-};
-
-bio.display = function() {
-
-    var formattedName = HTMLheaderName.replace("%data%", bio.name);
-    var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-    $("#header").prepend(formattedRole);
-    $("#header").prepend(formattedName);
-
-    var formattedmobile = HTMLmobile.replace("%data%", bio.contacts.phone_no);
-    $("#topContacts").append(formattedmobile);
-
-    var formattedgithub = HTMLgithub.replace("%data%", bio.contacts.github);
-    $("#topContacts").append(formattedgithub);
-
-    var formattedemail = HTMLemail.replace("%data%", bio.contacts.email);
-    $("#topContacts").append(formattedemail);
-
-    var formattedlocation = HTMLlocation.replace("%data%", bio.contacts.location);
-    $("#topContacts").append(formattedlocation);
-
-    var formattedbioPic = HTMLbioPic.replace("%data%", bio.bioPic);
-    $("#header").append(formattedbioPic);
-
-    $("#header").append(HTMLskillsStart);
-
-    for (var i = 0; i < bio.skills.length; i++) {
-        var formattedskills = HTMLskills.replace("%data%", bio.skills[i]);
-        $("#skills").append(formattedskills);
-    }
-
-    var formattedMsg = HTMLwelcomeMsg.replace("%data%", bio.welcome_message);
-    $("#skills").append(formattedMsg);
-
-    $("#footerContacts").append(formattedmobile, formattedemail, formattedgithub);
-}
-
-
-
-
-education.display = function() {
-    $("#education").append(HTMLschoolStart);
-    education.schools.forEach(function(i) {
-
-        var formattedschoolName = HTMLschoolName.replace("%data%", i.Name);
-        var formattedschoolDegree = HTMLschoolDegree.replace("%data%", i.Degree);
-        var formattedschoolLocation = HTMLschoolLocation.replace("%data%", i.location);
-        var formattedschoolMajor = HTMLschoolMajor.replace("%data%", i.major);
-
-        var formattedSchoolandDegree = formattedschoolName + formattedschoolDegree;
-
-        //$("#education").append(formattedschoolName);
-        $(".education-entry:last").append(formattedSchoolandDegree);
-        $(".education-entry:last").append(formattedschoolLocation);
-        $(".education-entry:last").append(formattedschoolMajor);
-    });
-}
-
-
-
-
-work.display = function() {
+    "display": function() {
     $("#workExperience").append(HTMLworkStart);
 
     work.jobs.forEach(function(job) {
@@ -182,58 +167,62 @@ work.display = function() {
     });
 }
 
+};
 
+var projects = {
+    "projects": [
 
-project.display = function() {
+        {
+            "title": " Designing a Responsive Portfolio Site ",
+            "dates": "2016",
+            "description": "To Design a portfolio site using responsive design",
+            "images"    :["images/project10_450.png","images/project11_450.png"]
+        },
+
+        {
+            "title": "Ultra Low Power SRAM Design Using 9nm TFET",
+            "dates": "2016",
+            "description": "To show TFET as superior alternative to MOSFET",
+            "images"    :["images/project20.png","images/project21.png"]
+        },
+
+        {
+            "title": "Traffic Congestion Detection in Arterial Road Using RF Technology",
+            "dates": "2015",
+            "description": "To reduce the traffic in Arterial Road using RF id and adaptive Algorithm",
+            "images"    :["images/project30.png","images/project31.png"]
+        }
+    ],
+    "display": function() {
     $("#projects").append(HTMLprojectStart);
 
-    project.projects.forEach(function(i) {
+    projects.projects.forEach(function(i) {
 
-
-
-        var formattedprojectTitle = HTMLprojectTitle.replace("%data%", i.title);
+       var formattedprojectTitle = HTMLprojectTitle.replace("%data%", i.title);
         $(".project-entry:last").append(formattedprojectTitle);
 
         var formattedprojectDates = HTMLprojectDates.replace("%data%", i.dates);
         $(".project-entry:last").append(formattedprojectDates);
 
-        var formattedprojectDescription = HTMLprojectDescription.replace("%data%", i.Description);
+        var formattedprojectDescription = HTMLprojectDescription.replace("%data%", i.description);
         $(".project-entry:last").append(formattedprojectDescription);
+
+       for(var j = 0 ; j < i.images.length; j++)
+         {
+        var formattedprojectImage = HTMLprojectImage.replace("%data%", i.images[j]);
+       $(".project-entry:last").append(formattedprojectImage);
+       }
     });
 
 }
 
-
-
-
-onlineclass.display = function() {
-    $(".education-entry:last").append(HTMLonlineClasses);
-    onlineclass.classes.forEach(function(i) {
-
-            var formattedonlineTitle = HTMLonlineTitle.replace("%data%", i.Title);
-            // $("#education").append(formattedonlineTitle);
-
-            var formattedonlineSchool = HTMLonlineSchool.replace("%data%", i.school);
-            var TitleandSchool = formattedonlineTitle + formattedonlineSchool;
-
-            $(".education-entry:last").append(TitleandSchool);
-
-            var formattedonlineDates = HTMLonlineDates.replace("%data%", i.date);
-            $(".education-entry:last").append(formattedonlineDates);
-
-            var formattedonlineURL = HTMLonlineURL.replace("%data%", i.URL);
-            $(".education-entry:last").append(formattedonlineURL);
-        }
-
-    );
-}
-
+};
 
 bio.display();
 education.display();
 work.display();
-project.display();
-onlineclass.display();
+projects.display();
+
 
 function inName(name) {
     var a = [];
